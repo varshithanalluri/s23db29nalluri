d1cc88c57f63aebfa70756946fa7b32213397428
varshithanalluri <113351765+varshithanalluri@users.noreply.github.com>
2023-11-10 02:32:36 -0600

    added lab

--- controllers/item.js
+++ controllers/item.js
@@ -12,9 +12,17 @@ exports.item_list = async function(req, res) {
    };
    
 // for a specific Costume.
-exports.item_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: item detail: ' + req.params.id);
-};
+exports.item_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await item.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle Costume create on POST.
 exports.item_create_post = async function(req, res) {
     console.log(req.body)
@@ -40,9 +48,24 @@ exports.item_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: item delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.item_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: item update PUT' + req.params.id);
-};
+exports.item_update_put =async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await item.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.item_name) toUpdate.item_name = req.body.item_name;
+    if(req.body.category) toUpdate.category = req.body.category;
+    if(req.body.quantity) toUpdate.quantity = req.body.quantity;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 exports.item_view_all_Page = async function(req, res) {
     try{
     theCostumes = await item.find();
