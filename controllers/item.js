04643baf0893e683d512c457a511c5eee1822a57
varshithanalluri <113351765+varshithanalluri@users.noreply.github.com>
2023-11-15 19:50:07 -0600

    added update

--- controllers/item.js
+++ controllers/item.js
@@ -111,4 +111,17 @@ res.status(500)
 res.send(`{'error': '${err}'}`);
 }
 };
+// Handle building the view for updating a costume.
+// query provides the id
+exports.item_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await item.findById(req.query.id)
+    res.render('itemupdate', { title: 'item Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
 
--- routes/items.js
+++ routes/items.js
@@ -5,4 +5,6 @@ var router = express.Router();
 router.get('/', item_controlers.item_view_all_Page );
 router.get('/detail', item_controlers.item_view_one_Page);
 router.get('/create', item_controlers.item_create_Page);
+router.get('/update', item_controlers.item_update_Page);
+
 module.exports = router;
